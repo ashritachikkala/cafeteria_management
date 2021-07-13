04461e239e679c9cb7d367ee8d9c0e74cc5137ff
ashritachikkala <ashritachikkala@gmail.com>
2021-07-13 16:06:29 +0530

    menu_items table migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  resources :menu_items
   resources :menu_categories
   resources :users
-  resources :create_users
   get "/" => "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
